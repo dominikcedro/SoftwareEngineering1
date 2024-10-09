8826ef67b8742a2321c42d442383d2cc1f8bd72e
Domin C <dominikcedro00@gmail.com>
2024-10-09 10:04:31 +0200

    UPDATE changed var types in add and sub methods of Calculator from INT to DOUBLE

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static double add(double a, double b) {
+        return a+b;
+    }
+    public static double sub(double a, double b) {
+        return a-b;
+    }
+}
